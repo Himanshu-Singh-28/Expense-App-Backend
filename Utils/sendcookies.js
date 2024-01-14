14878f4f8e434f951a520abfae4318aa68cdea1f
Himanshu Kumar Singh <himanshusingh1234h93@gmail.com>
2024-01-14 22:13:23 +0530

    4th commit

--- Utils/sendcookies.js
+++ Utils/sendcookies.js
@@ -4,7 +4,7 @@ export const sendcookie=(user,res,message,statusCode=200)=>{
     const token=jwt.sign({_id: user._id},process.env.JWT_SECRATE);
     res.status(statusCode).cookie("token",token,{
         httpOnly:true,
-        maxAge: 15*60*1000,
+        maxAge: 24*60*60*1000,
         sameSite: process.env.NODE_ENV==="Development"?"lax":"none",
         secure: process.env.NODE_ENV==="Development"?false: true,
     }).json({
